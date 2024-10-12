b53a7a563f13d375c71bb93e952f2123192ba48e
email <obansal.kunalo@gmail.com>
2024-10-12 23:31:26 +0530

    AuthContexxt updated

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -6,14 +6,13 @@ const AuthContext = createContext();
 const defaultAvatarUrl =
   "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"; // Default avatar
 // const API_URL = "http://localhost:5000";
-const API_URL = "https://auth-app-main-4bam.onrender.com";
+const API_URL = "https://auth-app-main-4bam.onrender.com" ;
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // const token = localStorage.getItem("token");
     const token = sessionStorage.getItem("token"); // Change to sessionStorage
 
     if (token) {
@@ -56,6 +55,7 @@ export const AuthProvider = ({ children }) => {
     sessionStorage.setItem("user", JSON.stringify(currUser)); // Change to sessionStorage
 
     setIsAuthenticated(true);
+    setUser(currUser); 
   };
   const logout = () => {
     sessionStorage.removeItem("token"); // Change to sessionStorage
